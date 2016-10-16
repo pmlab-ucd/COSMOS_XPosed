3e87253f0e5f4a88fffc7d47f2404b345b803e1c
Hao Fu <haofu@ucdavis.edu>
2016-10-15 19:26:26 -0700

    Write PScout file to SD card

--- app/src/main/java/fu/hao/cosmos_xposed/MainApplication.java
+++ app/src/main/java/fu/hao/cosmos_xposed/MainApplication.java
@@ -1,11 +1,17 @@
 package fu.hao.cosmos_xposed;
 
 import android.app.Application;
+import android.content.res.AssetManager;
+import android.os.Environment;
 import android.util.Log;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -20,152 +26,55 @@ import fu.hao.cosmos_xposed.hook.XMethod;
  * @since 10/14/2016
  */
 public class MainApplication extends Application {
-    private final String TAG = this.getClass().getSimpleName();
+    private final String TAG = this.getClass().getName();
 
-    private static List<String> PscoutMethod;
-    private static Set<XMethod> PscoutXMethod;
+    public static final String SENS_DEF_FILE = "test.txt";
+    public static final String SENS_DEF_FILE_PATH =
+            Environment.getExternalStorageDirectory().getAbsolutePath() + "/COSMOS/" + SENS_DEF_FILE;
 
-    static {
-        PscoutMethod = new ArrayList<>();
-        PscoutXMethod = new HashSet<>();
-    }
-
-    public static Set<XMethod> getPscoutXMethod() {
-        return PscoutXMethod;
-    }
-
-    public XMethod signature2Class(String sootSignature) throws ClassNotFoundException {
-        XMethod xMethod = new XMethod();
-        sootSignature = sootSignature.replace("<", "");
-        sootSignature = sootSignature.replace(">", "");
-        sootSignature = sootSignature.replace(")", "");
-        String[] splice = sootSignature.split(": ");
-        Class declaredClass = typeName2Class(splice[0]);
-        xMethod.setDeclaredClass(declaredClass);
-
-        splice = splice[1].split(" ");
-
-        splice = splice[1].split("\\(");
-
-        String methodName = splice[0];
-        xMethod.setMethodName(methodName);
-
-        if (splice.length > 1) {
-            splice = splice[1].split(",");
-            Class[] paramTypes = new Class[splice.length];
-            for (int i = 0; i < splice.length; i++) {
-                paramTypes[i] = typeName2Class(splice[i]);
+    private boolean copyAsset(AssetManager assetManager,
+                                     String fromAssetPath, String toPath) {
+        InputStream in = null;
+        OutputStream out = null;
+        try {
+            in = assetManager.open(fromAssetPath);
+            File newFile = new File(toPath);
+            if (newFile.exists()) {
+                return false;
             }
-            xMethod.setParamTypes(paramTypes);
-        }
-
-        return xMethod;
-    }
-
-    private Class typeName2Class(String typeName) throws ClassNotFoundException {
-        switch (typeName) {
-            case "int": return Integer.TYPE;
-            case "int[]": int[] integers = new int[1];
-                return integers.getClass();
-            case "short": return Short.TYPE;
-            case "short[]": short[] shorts = new short[1];
-                return shorts.getClass();
-            case "float": return Float.TYPE;
-            case "float[]": float[] floats = new float[1];
-                return floats.getClass();
-            case "double": return Double.TYPE;
-            case "double[]": double[] doubles = new double[1];
-                return doubles.getClass();
-            case "char": return Character.TYPE;
-            case "char[]": char[] chars = new char[1];
-                return chars.getClass();
-            case "byte": return Byte.TYPE;
-            case "byte[]": byte[] bytes = new byte[1];
-                return bytes.getClass();
-            default:
-                if (typeName.contains("[]")) {
-                    typeName = typeName.replace("[]", "");
-                    return Class.forName("[L" + typeName + ";");
-                } else {
-                    return Class.forName(typeName);
-                }
+            if (!newFile.getParentFile().exists()) {
+                newFile.getParentFile().mkdirs();
+            }
+            newFile.createNewFile();
+            out = new FileOutputStream(toPath);
+            copyFile(in, out);
+            in.close();
+            in = null;
+            out.flush();
+            out.close();
+            out = null;
+            Log.w(TAG, "Copy " + fromAssetPath + " to " + toPath);
+            return true;
+        } catch(Exception e) {
+            e.printStackTrace();
+            return false;
         }
     }
 
-    public Class<?> getArrayClass(Class<?> componentType) throws ClassNotFoundException{
-        ClassLoader classLoader = componentType.getClassLoader();
-        String name;
-        if(componentType.isArray()){
-            // just add a leading "["
-            name = "["+componentType.getName();
-        }else if(componentType == boolean.class){
-            name = "[Z";
-        }else if(componentType == byte.class){
-            name = "[B";
-        }else if(componentType == char.class){
-            name = "[C";
-        }else if(componentType == double.class){
-            name = "[D";
-        }else if(componentType == float.class){
-            name = "[F";
-        }else if(componentType == int.class){
-            name = "[I";
-        }else if(componentType == long.class){
-            name = "[J";
-        }else if(componentType == short.class){
-            name = "[S";
-        }else{
-            // must be an object non-array class
-            name = "[L"+componentType.getName()+";";
+    private void copyFile(InputStream in, OutputStream out) throws IOException {
+        byte[] buffer = new byte[1024];
+        int read;
+        while((read = in.read(buffer)) != -1){
+            out.write(buffer, 0, read);
         }
-        return classLoader != null ? classLoader.loadClass(name) : Class.forName(name);
     }
 
     @Override
     public void onCreate() {
         super.onCreate();
 
-        BufferedReader reader = null;
-        try {
-            reader = new BufferedReader(
-                    new InputStreamReader(getAssets().open("test.txt")));
-
-            // do reading, usually loop until end of file reading
-            String mLine;
-            while ((mLine = reader.readLine()) != null) {
-                //process line
-                if (mLine.startsWith("<")) {
-                    PscoutMethod.add(mLine);
-                }
-            }
-        } catch (IOException e) {
-            //log the exception
-            e.printStackTrace();
-        } finally {
-            if (reader != null) {
-                try {
-                    reader.close();
-                } catch (IOException e) {
-                    //log the exception
-                }
-            }
-        }
-
-        for (String sensSignature : PscoutMethod) {
-            try {
-                XMethod xMethod = signature2Class(sensSignature);
-                if (xMethod != null) {
-                    Log.w(TAG, xMethod.getMethodName() + ": " + xMethod.getDeclaredClass());
-                    if (xMethod.getParamTypes() != null) {
-                        for (Class paramType : xMethod.getParamTypes()) {
-                            Log.w(TAG, "paramType" + paramType);
-                        }
-                    }
-                    PscoutXMethod.add(xMethod);
-                }
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
-            }
-        }
+        Log.e(TAG, "COSMOS start!");
+        Log.w(TAG, "Copying asset ");
+        copyAsset(getAssets(), SENS_DEF_FILE, SENS_DEF_FILE_PATH);
     }
 }
--- app/src/main/java/fu/hao/cosmos_xposed/hook/Main.java
+++ app/src/main/java/fu/hao/cosmos_xposed/hook/Main.java
@@ -3,21 +3,26 @@ package fu.hao.cosmos_xposed.hook;
 import android.app.Application;
 import android.app.PendingIntent;
 import android.os.Build;
+import android.os.Environment;
 import android.telephony.SmsManager;
 import android.util.Log;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Member;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
@@ -35,7 +40,137 @@ import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
  * @since 10/13/2016
  */
 public class Main implements IXposedHookLoadPackage {
-    private final String TAG = this.getClass().getSimpleName();
+    private final String TAG = this.getClass().getName();
+
+    private static List<String> PscoutMethod;
+    private static Set<XMethod> PscoutXMethod;
+
+    static {
+        PscoutMethod = new ArrayList<>();
+        PscoutXMethod = new HashSet<>();
+    }
+
+    public static Set<XMethod> getPscoutXMethod() {
+        return PscoutXMethod;
+    }
+
+    public XMethod signature2Class(String sootSignature) throws ClassNotFoundException {
+        XMethod xMethod = new XMethod();
+        sootSignature = sootSignature.replace("<", "");
+        sootSignature = sootSignature.replace(">", "");
+        sootSignature = sootSignature.replace(")", "");
+        String[] splice = sootSignature.split(": ");
+        Class declaredClass = typeName2Class(splice[0]);
+        xMethod.setDeclaredClass(declaredClass);
+
+        splice = splice[1].split(" ");
+
+        splice = splice[1].split("\\(");
+
+        String methodName = splice[0];
+        xMethod.setMethodName(methodName);
+
+        if (splice.length > 1) {
+            splice = splice[1].split(",");
+            Class[] paramTypes = new Class[splice.length];
+            for (int i = 0; i < splice.length; i++) {
+                paramTypes[i] = typeName2Class(splice[i]);
+            }
+            xMethod.setParamTypes(paramTypes);
+        }
+
+        return xMethod;
+    }
+
+    private Class typeName2Class(String typeName) throws ClassNotFoundException {
+        switch (typeName) {
+            case "int":
+                return Integer.TYPE;
+            case "int[]":
+                int[] integers = new int[1];
+                return integers.getClass();
+            case "short":
+                return Short.TYPE;
+            case "short[]":
+                short[] shorts = new short[1];
+                return shorts.getClass();
+            case "float":
+                return Float.TYPE;
+            case "float[]":
+                float[] floats = new float[1];
+                return floats.getClass();
+            case "double":
+                return Double.TYPE;
+            case "double[]":
+                double[] doubles = new double[1];
+                return doubles.getClass();
+            case "char":
+                return Character.TYPE;
+            case "char[]":
+                char[] chars = new char[1];
+                return chars.getClass();
+            case "byte":
+                return Byte.TYPE;
+            case "byte[]":
+                byte[] bytes = new byte[1];
+                return bytes.getClass();
+            default:
+                if (typeName.contains("[]")) {
+                    typeName = typeName.replace("[]", "");
+                    return Class.forName("[L" + typeName + ";");
+                } else {
+                    return Class.forName(typeName);
+                }
+        }
+    }
+
+    private void readSensDefFile() {
+        Log.w(TAG, "Read SENS_DEF_File from " + MainApplication.SENS_DEF_FILE);
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(
+                    new InputStreamReader(
+                            new FileInputStream(
+                                    MainApplication.SENS_DEF_FILE_PATH)));
+
+            // do reading, usually loop until end of file reading
+            String mLine;
+            while ((mLine = reader.readLine()) != null) {
+                //process line
+                if (mLine.startsWith("<")) {
+                    PscoutMethod.add(mLine);
+                }
+            }
+        } catch (IOException e) {
+            //log the exception
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    //log the exception
+                }
+            }
+        }
+
+        for (String sensSignature : PscoutMethod) {
+            try {
+                XMethod xMethod = signature2Class(sensSignature);
+                if (xMethod != null) {
+                    Log.w(TAG, xMethod.getMethodName() + ": " + xMethod.getDeclaredClass());
+                    if (xMethod.getParamTypes() != null) {
+                        for (Class paramType : xMethod.getParamTypes()) {
+                            Log.w(TAG, "paramType" + paramType);
+                        }
+                    }
+                    PscoutXMethod.add(xMethod);
+                }
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
     /**
      * 包加载时候的回调, which is the entry method of the hook system
@@ -45,8 +180,12 @@ public class Main implements IXposedHookLoadPackage {
         Log.v(TAG, "Package checking: " + lpparam.packageName);
         // 将包名不是 edu.ucdavis.test的应用剔除掉
         //if (!lpparam.packageName.equals("edu.ucavis.test")) {
-            //return;
+        //return;
         //}
+
+        if (getPscoutXMethod() == null) {
+            readSensDefFile();
+        }
 
         XposedBridge.log("Loaded app: " + lpparam.packageName);
         Log.w(TAG, "Staring hooking " + lpparam.packageName);
@@ -56,13 +195,17 @@ public class Main implements IXposedHookLoadPackage {
             return;
         }
 
-        for (XMethod xMethod : MainApplication.getPscoutXMethod()) {
+        for (XMethod xMethod : getPscoutXMethod()) {
+            Log.w(TAG, xMethod.getMethodName() + " " + xMethod.getDeclaredClass());
+            for (Class paramType : xMethod.getParamTypes()) {
+                Log.w(TAG, "paramType" + paramType);
+            }
             findAndHookMethod(xMethod.getDeclaredClass(), xMethod.getMethodName(), xMethod.getParamTypes(),
                     new XC_MethodHook() {
                         @Override
                         protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
                             XposedBridge.log("开始劫持了~");
-                            Log.w(TAG, "Staring hooking!");
+                            Log.w(TAG, "Hooking method " + param.method);
                             param.args[0] = "10086";
                         }
 
@@ -70,9 +213,40 @@ public class Main implements IXposedHookLoadPackage {
                         protected void afterHookedMethod(MethodHookParam param) throws Throwable {
                             XposedBridge.log("劫持结束了~");
                             XposedBridge.log("参数1 = " + param.args[0]);
+
+                            Log.w(TAG, "End hooking method " + param.method);
                         }
                     });
         }
+    }
+
+    public Class<?> getArrayClass(Class<?> componentType) throws ClassNotFoundException {
+        ClassLoader classLoader = componentType.getClassLoader();
+        String name;
+        if (componentType.isArray()) {
+            // just add a leading "["
+            name = "[" + componentType.getName();
+        } else if (componentType == boolean.class) {
+            name = "[Z";
+        } else if (componentType == byte.class) {
+            name = "[B";
+        } else if (componentType == char.class) {
+            name = "[C";
+        } else if (componentType == double.class) {
+            name = "[D";
+        } else if (componentType == float.class) {
+            name = "[F";
+        } else if (componentType == int.class) {
+            name = "[I";
+        } else if (componentType == long.class) {
+            name = "[J";
+        } else if (componentType == short.class) {
+            name = "[S";
+        } else {
+            // must be an object non-array class
+            name = "[L" + componentType.getName() + ";";
+        }
+        return classLoader != null ? classLoader.loadClass(name) : Class.forName(name);
     }
 
 
